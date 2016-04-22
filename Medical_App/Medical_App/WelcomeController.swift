19c921f4f866e76ef53b5a4d115ec3453fc51f03
Gregory Johnson <gsoccer111@gmail.com>
2016-04-22 16:26:37 -0400

    Birthday input
    Changed the user plist file to save year, month, and day of birth
    separately. Utilizes a picker to choose your birthday

--- Medical_App/Medical_App/WelcomeController.swift
+++ Medical_App/Medical_App/WelcomeController.swift
@@ -22,13 +22,17 @@ class WelcomeController: UIViewController, UITextFieldDelegate
     @IBOutlet weak var b_back: UIButton!
     @IBOutlet weak var b_done: UIButton!
     
+    @IBOutlet weak var p_datePicker: UIDatePicker!
+    
     let FirstNameKey = "First Name"
     let MiddleInitialKey = "Middle Name"
     let LastNameKey = "Last Name"
-    let BirthdayKey = "Birthday"
+    let BirthYearKey = "Birth Year"
+    let BirthMonthKey = "Birth Month"
+    let BirthDayKey = "Birth Day"
     
     let cornRad:CGFloat = 25        //Corner radius for the buttons in the view
-    var inputCount:Int = 0
+    var stepNum:Int = 0
     
     override func viewDidLoad()
     {
@@ -77,6 +81,11 @@ class WelcomeController: UIViewController, UITextFieldDelegate
         self.b_done.layer.cornerRadius = self.cornRad
         self.b_back.alpha = 0
         self.b_done.alpha = 0
+        
+        self.p_datePicker.alpha = 0
+        self.p_datePicker.backgroundColor = UIColor.whiteColor()
+        self.p_datePicker.layer.cornerRadius = self.cornRad
+        self.p_datePicker.layer.masksToBounds = true;
     }
     
     
@@ -166,6 +175,61 @@ class WelcomeController: UIViewController, UITextFieldDelegate
             })
     }
     
+    func fadeOutName() -> Void
+    {
+        self.b_done.enabled = false
+        self.b_back.enabled = false
+        UIView.animateWithDuration(0.5,
+            delay: 0.25,
+            options: .CurveEaseIn,
+            animations: {
+                self.l_welcome.alpha = 0
+                
+                self.i_textInput1.alpha = 0
+                self.i_textInput2.alpha = 0
+                self.i_textInput3.alpha = 0
+                self.l_title1.alpha = 0
+                self.l_title2.alpha = 0
+                self.l_title3.alpha = 0
+            },
+            completion: { finished in
+                self.l_welcome.text = "Birthday"
+                self.i_textInput1.text = ""
+                self.i_textInput2.text = ""
+                self.i_textInput3.text = ""
+                self.fadeInBirthday()
+            })
+    }
+    
+    func fadeInBirthday() -> Void
+    {
+        UIView.animateWithDuration(0.5,
+            delay: 0.0,
+            options: .CurveEaseIn,
+            animations: {
+                self.l_welcome.alpha = 1
+
+                self.p_datePicker.alpha = 1
+            },
+            completion: { finished in
+                self.b_done.enabled = true
+                self.b_back.enabled = true
+                print(g_plist!.getValuesInPlistFile())
+                print("Done moving and fading")
+            })
+    }
+    
+    func nextStep()
+    {
+        switch stepNum
+        {
+        case 1:
+            fadeOutName()
+        default:
+            print("stepNum is out of bounds")
+        }
+    }
+    
     func textFieldShouldReturn(textField: UITextField) -> Bool {
         self.view.endEditing(true)
         return false
@@ -177,10 +241,20 @@ class WelcomeController: UIViewController, UITextFieldDelegate
          if(g_plist != nil)
          {
             let dict = g_plist!.getMutablePlistFile()!
-            dict[FirstNameKey] = self.i_textInput1.text
-            dict[MiddleInitialKey] = self.i_textInput2.text
-            dict[LastNameKey] = self.i_textInput3.text
-         
+            switch stepNum
+            {
+            case 0:
+                dict[FirstNameKey] = self.i_textInput1.text
+                dict[MiddleInitialKey] = self.i_textInput2.text
+                dict[LastNameKey] = self.i_textInput3.text
+            case 1:
+                let components = self.p_datePicker.calendar.components([.Year, .Month, .Day], fromDate: p_datePicker.date)
+                dict[BirthMonthKey] = components.month
+                dict[BirthDayKey] = components.day
+                dict[BirthYearKey] = components.year
+            default:
+                print("stepNum is out of bounds")
+            }
          do {
             try g_plist?.addValuesToPlistFile(dict)
          } catch {
@@ -188,6 +262,9 @@ class WelcomeController: UIViewController, UITextFieldDelegate
          }
             print(g_plist!.getValuesInPlistFile())
          }
+        
+        stepNum += 1
+        nextStep()
     }
     
     //Back clicked
